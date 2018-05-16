62fa72e2bdc7a8d464697c830b9b7647faa2b1a7
SkHawk <alexchamon@poli.ufrj.br>
2018-05-16 18:56:10 -0300

    Adicionado a lib game.h

--- genius.c
+++ genius.c
@@ -2,32 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void aleatorio(){
-	char randChar = ' ';
-	int counter = 0;
-	int randNum = 0;
-
-	// Provide seed for the random number
-	// generator.
-	srand(time(NULL));
-	for (counter = 0; counter < 10; counter++)
-	{
-		// First, pick a number between 0 and
-		// 25.
-		randNum = 26 * (rand() / (RAND_MAX + 1.0));
-
-		// The ASCII code for ‘a’ is 97, so
-		// add 97 to the random number to get
-		// a lower case letter.
-		randNum = randNum + 97;
-
-		// Type cast to character to convert
-		// it to a char type.
-		randChar = (char) randNum;
-
-		printf ("Random char: %c\n", randChar);
-	}
-}
+#include "libs/game.h"
 
 int main(void){
 
